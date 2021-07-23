8fcc7d665e1aa2f9480c1c3c811088e39656f0b1
Tri The Wing Man <tri.doan@sjsu.edu>
2021-07-23 01:06:00 -0700

    Add files via upload

--- problems.py
+++ problems.py
@@ -225,3 +225,50 @@ def reverseList(self, head: ListNode) -> ListNode:
         prev = curr
         curr = temp
     return prev
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+def hasCycle(self, head: ListNode) -> bool:
+    dictionary = {}
+    """
+    key/value hashmap
+    store head value in the dictionary
+
+
+    """
+    while head:
+        if head in dictionary:
+            return True
+        dictionary[head] = True
+        head = head.next
+    return False
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    dummy = curr = ListNode(0)
+    while l1 and l2:
+        if l1.val < l2.val:
+            curr.next = l1
+            l1 = l1.next
+        else:
+            curr.next = l2
+            l2 = l2.next
+        curr = curr.next
+    while l1:
+        curr.next = l1
+        l1 = l1.next
+        curr = curr.next
+    while l2:
+        curr.next = l2
+        l2 = l2.next
+        curr = curr.next
+    return dummy.next
