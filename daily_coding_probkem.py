07b9c5e323e25e8027ade94f991df64a589fc204
Tri Doan <tridoan1998@gmail.com>
2021-10-01 15:15:28 -0700

    Add files via upload

--- daily_coding_probkem.py
+++ daily_coding_probkem.py
@@ -1,10 +1,23 @@
-def sell_stock(stocks):
-    lowest, highest = stocks[0], 0
-    for i  in range(1, len(stocks)):
-        lowest = min(lowest, stocks[i])
-        if stocks[i] - lowest > highest:
-            highest = stocks[i] - lowest
-    return highest
+def two_sum( nums, k):
+    if len(nums) <= 1:
+        return -1
+    else:
+        hm = {}
+        for i in range(len(nums)):
+            if nums[i] not in hm.values():
+                hm[i] = nums[i]
+           # else:
+            #    hm[i] += 1
+        temp = 0
+        for i in range(len(nums)):
+            temp = k - nums[i]
+            print(temp)
+            if temp == hm.values():
+                return [i, hm.keys(temp)]
+        return hm
 
 
 
+nums = [2, 7, 11, 15]
+k = 9
+print(two_sum(nums, k))
