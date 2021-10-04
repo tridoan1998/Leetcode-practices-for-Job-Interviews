61f8de5d28a75a998b8646c05530939c6c4a598e
Tri Doan <tridoan1998@gmail.com>
2021-10-04 12:39:26 -0700

    Add files via upload

--- daily_coding_probkem.py
+++ daily_coding_probkem.py
@@ -1,23 +1,74 @@
-def two_sum( nums, k):
-    if len(nums) <= 1:
-        return -1
-    else:
-        hm = {}
-        for i in range(len(nums)):
-            if nums[i] not in hm.values():
-                hm[i] = nums[i]
-           # else:
-            #    hm[i] += 1
-        temp = 0
-        for i in range(len(nums)):
-            temp = k - nums[i]
-            print(temp)
-            if temp == hm.values():
-                return [i, hm.keys(temp)]
-        return hm
+
+#linked list , infinate,
+"""
+store integer,
+deleteme() => take Null pointer => delete that value from the linkedlist
+
+linkedlis: val, next, previous
+
+class linkedlist:
+    self __init__(self, val, next):
+        self.val = val
+        self.next = next
+7 => 5 => 4 => 3 ...
+     ^
+   node
+   node.next = 4
+   node.val = 5
+
+   node.val = node.next
+7 => 4 => 4 => 3 ...
+     ^
+     node
+     node.next = 4
+     node.next.next.val = 3
+     node = node.next.next
+7 => 4 => 3  ...
+
+
+def deleteme(node):
+    #copy the 4 to the 5
+    node.val = node.next.val
+    #7 => 4 => 4 => 3 ...
+
+    #delete the old 4
+    node.next = node.next.next
 
 
 
-nums = [2, 7, 11, 15]
-k = 9
-print(two_sum(nums, k))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+val: 7
+next: 5
+
+
+temp pointer point to 7
+run a loop iterate through the linkedlist
+temp = temp.next
+if temp.val == value:
+    #remove
+else:
+    continue
+
+
+
+
+
+
+7 => 4 => 3
+"""
+
+def deleteme():
